184b33706c1cec824794b0ab763c549beebe945b
Erwan Martin <erwan.martin666@gmail.com>
2025-02-26 16:40:10 +0100

    first tests with pygame env +gitignore

---
+++ pygame_test.py
@@ -0,0 +1,33 @@
+from Simulation.Environment import Environment
+from Simulation import Agents
+
+from PIL import Image
+
+
+SIMULATION_NAME = 'Multi-Robot-Exploration'
+EXPERIMENT_NAME = 'multi_robot / Example 01'
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+BACKGROUND_COLOR = (200, 200, 200)
+
+ENV_IMAGE_PATH = "/home/erwan/Documents/tests_simulations/Pygame/test_500_500.png"
+
+NB_GROUND_AGENTS = 100
+
+img = Image.open(ENV_IMAGE_PATH).convert("L")
+
+
+def main():
+    env = Environment(background_color= BACKGROUND_COLOR, width= SCREEN_WIDTH, height=SCREEN_HEIGHT)
+
+
+    for i in range(NB_GROUND_AGENTS):
+        env.add_agent(Agents.Ground(env=env))
+        
+    
+    env.run()
+
+if __name__ == "__main__":
+    main()
