f0a32af1a6d65f0af4dd9c62b00d09dec151d3df
Erwan Martin <erwan.martin666@gmail.com>
2025-03-26 16:22:34 +0100

    cleaning examples -> 0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RAPID',
-    version='0.6',
+    version='0.7',
     packages=find_packages(),
     description='Robotics Agent Prototyping for Intelligence Development',
     author='Erwan MARTIN',
