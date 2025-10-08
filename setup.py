990fb1e0d92346792cc04400f1bea64318b572a0
Erwan Martin <erwan.martin666@gmail.com>
2025-10-08 15:31:43 +0200

    0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RAPID',
-    version='0.10',
+    version='0.11',
     packages=find_packages(),
     description='Robotics Agent Prototyping for Intelligence Development',
     author='Erwan MARTIN',
